087717932161911fdcc3f63d21ad2ffa98daa3b8
Yor Abahs <ryshaba@gmail.com>
2022-12-25 15:35:49 -0500

    Refactor README file

--- README.md
+++ README.md
@@ -6,6 +6,4 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
+This project aims to give real-world scenarios of manipulating the DOM. The functionality serves two purposes: appending dynamically added data to the DOM, and using javascript to improve the usability of an otherwise static site.
--- js/app.js
+++ js/app.js
@@ -13,36 +13,70 @@
  *
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
 
 /**
  * Define Global Variables
  *
 */
-
+let sections = document.getElementsByTagName('section')
+let navDiv = document.getElementById('navbar__list')
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
+// myMenu function takes no input and dynamically creates the nav menu based on number of unique sections
+let myMenu = () => {
+  let menuArray = [];
+  for(const section of sections){
+    let myButton = `<li class='menu__link'><a href=#${strFix(section.dataset.nav)}>${section.dataset.nav}</a></li>`;
+    menuArray.push(myButton)
+  }
+  return menuArray.join('')
+}
 
+// isInViewport function takes an element as input checks if it's in viewport
+let isInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
+// strFix takes a string as input and returns the same string in lowercase and no space.
+let strFix = (string) => {
+  let newStr = string.toLowerCase().replaceAll(' ','')
+  return newStr;
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  *
 */
-
+//renderMenu function adds the nav menu to the DOM
+let renderMenu  = (parentDiv) => {
+  parentDiv.innerHTML = myMenu();
+}
+renderMenu(navDiv)
 // build the nav
 
 
 // Add class 'active' to section when near top of viewport
-
+document.addEventListener('scroll',() => {
+  for (let section of sections){
+    if (isInViewport(section)){
+      section.classList.add('active')
+    }
+    else{
+      section.classList.remove('active')
+    }
+  }
+});
 
 // Scroll to anchor ID using scrollTO event
 
